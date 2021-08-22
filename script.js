07c005d0b1554625ed94b8c34cc9c0fa2f0a52b2
Pfizz <pfiste80@gmail.com>
2021-08-22 11:40:45 -0400

    intervals

--- script.js
+++ script.js
@@ -6,7 +6,45 @@ $('.saveBtn').on("click", function() {
     var time = $(this).parent().attr("id");
     localStorage.setItem(time, tasks); 
   });
-});
+
+
+
+// current time
+
+var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+$("#currentDay").append(currentTime);
+
+
+// logic for color changing
+
+function hourUpdater() {
+    // get current number of hours
+    var currentHour = moment().hours();
+
+    // loop over time blocks
+    $('.time-block').each(function() {
+      var blockHour = parseInt(
+        $(this)
+          .attr('id')
+          .split('-')[1]
+      );
+
+      // check if we've moved past this time
+      if (blockHour < currentHour) {
+        $(this).addClass('past');
+      } else if (blockHour === currentHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      }
+    });
+  }
+  var interval = setInterval(hourUpdater, 15000);
+  hourUpdater();
+
 
 // loads data from local storage
 $("#9AM .description").val(localStorage.getItem('9AM'));
@@ -20,11 +58,4 @@ $("#4PM .description").val(localStorage.getItem('4PM'));
 $("#5PM .description").val(localStorage.getItem('5PM'));
 
 
-// current time
-
-var currentTime = moment().format('MMMM Do YYYY, h:mm:ss a');
-$("#currentDay").append(currentTime);
-
-
-// logic for color changing
-
+});
